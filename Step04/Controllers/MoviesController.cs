c6ee93ad9b07c14497455557632475b59cf9d373
ukaracivio <umit.karacivi@outlook.com>
2024-05-22 11:54:23 +0300

    Some changes

--- Step04/Controllers/MoviesController.cs
+++ Step04/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Interfaces;
 using eTickets.Models;
+using eTickets.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -51,28 +52,102 @@ namespace eTickets.Controllers
 
             // Controllerdan Create View tarafına oluşan dropdownlist bilgilerini aktarmam lazım ki görünsünler.
 
-            ViewBag.Cinema = new SelectList(movieDropDownsData.Cinemas, "Id", "Name");
+            ViewBag.Cinemas = new SelectList(movieDropDownsData.Cinemas, "Id", "Name");
 
-            ViewBag.Producer = new SelectList(movieDropDownsData.Producers, "Id", "FullName");
+            ViewBag.Producers = new SelectList(movieDropDownsData.Producers, "Id", "FullName");
 
-            ViewBag.Actor = new SelectList(movieDropDownsData.Actors, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropDownsData.Actors, "Id", "FullName");
 
             return View();
         }
 
         //Post: Movies/Create
-        //[HttpPost]
-        //public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Movie movie)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(movie);
-        //    }
+        [HttpPost]
+        public async Task<IActionResult> Create(NewMovieVM movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Herhangi bir post anında eğer sayfada modelstate valid değilse buraya düşecek. Ama ViewBag in yapısı gereği post gibi işlemlerde içeriği siliniyor. dolayısı ile tekrardan oluşturacağız.
 
-        //    //_service.Add(actor);
-        //    // (23)
-        //    await _service.AddAsync(movie);
+                var movieDropDownsData = await _service.GetNewMovieDropdownsValues();
 
-        //}
+                // Controllerdan Create View tarafına oluşan dropdownlist bilgilerini aktarmam lazım ki görünsünler.
+
+                ViewBag.Cinemas = new SelectList(movieDropDownsData.Cinemas, "Id", "Name");
+
+                ViewBag.Producers = new SelectList(movieDropDownsData.Producers, "Id", "FullName");
+
+                ViewBag.Actors = new SelectList(movieDropDownsData.Actors, "Id", "FullName");
+
+                return View(movie);
+            }
+
+            await _service.AddNewMovieAsync(movie);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // 42
+        // Get: Moviess/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movieDetails = await _service.GetMovieByIdAsync(id); // var mı/yok mu
+
+            if (movieDetails == null) return View("NotFound");
+
+            var data = new NewMovieVM()
+            {
+                Id = movieDetails.Id,
+                Name = movieDetails.Name,
+                Description = movieDetails.Description,
+                Price = movieDetails.Price,
+                StartDate = movieDetails.StartDate,
+                EndDate = movieDetails.EndDate,
+                ImageURL = movieDetails.ImageURL,
+                MovieCategory = movieDetails.movieCategory,//
+                CinemaId = movieDetails.CinemaId,
+                ProducerId = movieDetails.ProducerId,
+                ActorIds = movieDetails.Actors_Movies.Select(acmo => acmo.ActorId).ToList()
+            };
+
+            // DropdownList
+            var movieDropDownsData = await _service.GetNewMovieDropdownsValues();
+
+            // Controllerdan Create View tarafına oluşan dropdownlist bilgilerini aktarmam lazım ki görünsünler.
+
+            ViewBag.Cinemas = new SelectList(movieDropDownsData.Cinemas, "Id", "Name");
+
+            ViewBag.Producers = new SelectList(movieDropDownsData.Producers, "Id", "FullName");
+
+            ViewBag.Actors = new SelectList(movieDropDownsData.Actors, "Id", "FullName");
+
+            return View(data);
+        }
+
+        // Post
+        [HttpPost] // View tarafından gönderilecek verileri yakalamak için
+        public async Task<IActionResult> Edit(int id, NewMovieVM movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var movieDropDownsData = await _service.GetNewMovieDropdownsValues();
+
+                // Controllerdan Create View tarafına oluşan dropdownlist bilgilerini aktarmam lazım ki görünsünler.
+
+                ViewBag.Cinemas = new SelectList(movieDropDownsData.Cinemas, "Id", "Name");
+
+                ViewBag.Producers = new SelectList(movieDropDownsData.Producers, "Id", "FullName");
+
+                ViewBag.Actors = new SelectList(movieDropDownsData.Actors, "Id", "FullName");
+
+                return View(movie);
+            }
+
+            await _service.UpdateMovieAsync(movie); // Servisde çalışacak kısım
+
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
