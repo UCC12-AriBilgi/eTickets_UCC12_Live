f7575c019c82d4b97147ec78682d3de3e21665b5
aribilgiogretmen <aribilgiogretmen@gmail.com>
2024-05-27 14:15:53 +0300

    Some changes

---
+++ Step06/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    // 57
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+
+        //58
+        public List<OrderItem> OrderItems { get; set; }
+
+    }
+}
---
+++ Step06/Models/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    // 57
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public double Price { get; set; }
+
+        public int MovieId { get; set; }
+
+        // Relation 
+        [ForeignKey("MovieId")]
+        public Movie Movie { get; set; }
+
+        // 58
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+    }
+}
