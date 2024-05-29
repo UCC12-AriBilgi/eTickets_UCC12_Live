b3ef05fedbf9c4b65f47c0c759646231f6b61255
aribilgiogretmen <aribilgiogretmen@gmail.com>
2024-05-29 13:37:46 +0300

    Some changes

---
+++ Step06/Data/Interfaces/IOrdersService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Interfaces
+{
+    public interface IOrdersService
+    {
+        // 72
+
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsyncs(string userId, string userRole);
+    }
+}
---
+++ Step06/Data/Services/OrdersService.cs
@@ -0,0 +1,65 @@
+﻿using eTickets.Data.Interfaces;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        // 72
+
+        public readonly AppDbContext _context;
+
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<List<Order>> GetOrdersByUserIdAndRoleAsyncs(string userId, string userRole)
+        {
+            // Kullanıcı Id ve Rolune göre kayıt getirecek metot
+
+            var orders=await _context.Orders
+                .Include(o=> o.OrderItems)
+                .ThenInclude(o=> o.Movie)
+                .Where(o=> o.UserId== userId)
+                .ToListAsync();
+
+
+            return orders;
+        }
+
+        public async Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            // ShoppingCart tarafındakileri VT tarafına gönderme
+
+            // Sipariş kaydı oluşturuluyor.(Master kayıt)
+            var order = new Order
+            {
+                UserId = userId,
+                Email = userEmailAddress
+            };
+
+            await _context.Orders.AddAsync(order);
+
+            await _context.SaveChangesAsync();
+
+            foreach (var item in items)
+            {
+                var orderItem = new OrderItem
+                {
+                    Amount = item.Amount,
+                    MovieId = item.Movie.Id,
+                    Price = item.Movie.Price,
+                    OrderId = order.Id
+                };
+
+                await _context.OrderItems.AddAsync(orderItem);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
